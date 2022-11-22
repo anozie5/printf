577a40973be32883cbe73833fdd87d93f4955d0c
anozie5 <chinemeremanozie5@gmail.com>
2022-11-22 14:15:04 -0800

    My task-14.c

---
+++ task-14.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+*func_R - The function to be called when a 'R'
+*character id given
+*@ap: the va_list variable
+*@count: the length of the string to be printed
+*Return: 1
+*/
+
+int func_R(va_list *ap, int *count)
+{
+	char *str;
+
+	char n[] = "null";
+
+	str = va_arg(*ap, char *);
+	if (str == NULL)
+		*count += rot13(n);
+	else
+		*count += rot13(str);
+	return (1);
+}
+
